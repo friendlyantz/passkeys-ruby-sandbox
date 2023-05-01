5b357a5a3d56afcf92aef01b40a1db07eefca5fa
Anton Panteleev <friendlyantz@pm.me>
2023-05-01 22:09:52 +1000

    rails g controller home index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # get 'home/index'
+  root to: "home#index"
+
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

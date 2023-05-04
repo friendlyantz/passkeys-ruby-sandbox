b5288a5336b5dd0061b6268a0c56198467c7ebb5
Anton Panteleev <friendlyantz@pm.me>
2023-05-04 12:03:29 +1000

    add webauthn_id column to users. rerun migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  after_initialize do
+    self.webauthn_id ||= WebAuthn.generate_user_id
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2023_05_01_115642) do
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "webauthn_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

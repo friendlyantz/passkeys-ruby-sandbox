c84dc4d376fc8d712e28ed9554109e1b50971ef5
Anton Panteleev <friendlyantz@pm.me>
2023-05-05 15:18:27 +1000

    Init User object and WebAuthn options for create Cred

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,5 +3,16 @@ class RegistrationsController < ApplicationController
   end
 
   def create
+    user = User.new(username: params[:username])
+    options = WebAuthn::Credential.options_for_create(
+      user: {
+        id: user.webauthn_id, # id is required for user
+        name: user.username, # name is required for user
+      }
+    )
+
+    binding.pry
+    # TODO
+    
   end
 end

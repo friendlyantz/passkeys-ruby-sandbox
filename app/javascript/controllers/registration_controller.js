410af0d52e073571f3d387f1887c78d7b7e23684
Anton Panteleev <friendlyantz@pm.me>
2023-05-27 13:59:18 +1000

    Generate Stimulus controller and ImportMap packages
    rails g stimulus registration
    bin/importmap pin @github/webauthn-json
    bin/importmap pin @rails/request.js

---
+++ app/javascript/controllers/registration_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus"
+import * as WebAuthnJSON from "@github/webauthn-json"
+import { FetchRequest } from "@rails/request.js"
+
+// Connects to data-controller="registration"
+export default class extends Controller {
+  static targets = ["nickname"]
+  static values = { callback: String }
+
+  create(event) {
+    const [data, status, xhr] = event.detail;
+    const _this = this
+
+    WebAuthnJSON.create({ "publicKey": data }).then(async function (credential) {
+      const request = new FetchRequest("post", _this.callbackValue + `?nickname=${_this.nicknameTarget.value}`, { body: JSON.stringify(credential), responseKind: "turbo-stream" })
+      await request.perform()
+    }).catch(function (error) {
+      console.log("something is wrong", error);
+    });
+  }
+
+  error(event) {
+    console.log("something is wrong", event);
+  }
+}
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@github/webauthn-json", to: "https://ga.jspm.io/npm:@github/webauthn-json@2.1.1/dist/esm/webauthn-json.js"
+pin "@rails/request.js", to: "https://ga.jspm.io/npm:@rails/request.js@0.0.8/src/index.js"
